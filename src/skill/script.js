aeaef5c07792bbeae1d21ec66eb5bc55e1824a0b
unknown <alex.herbert.tech@gmail.com>
2023-10-20 15:49:56 +1300

    Made some more changes to skills page, few more tweaks and it should be good

--- src/skill/script.js
+++ src/skill/script.js
@@ -1,18 +1,33 @@
 function toggleSkillDetails(element) {
+    // Get all logo elements
+    const allLogos = document.querySelectorAll('.js-logo, .html-logo, .css-logo, .sql-logo'); // Add other logo class selectors as needed
+
     // Get the sibling 'js-details' container of the clicked logo
     const details = element.nextElementSibling;
 
-    // Toggle the opacity, visibility, and position properties
+    // If the clicked logo's details are currently hidden, show them and hide other logos
     if (details.style.opacity === "0" || details.style.opacity === "") {
         details.style.opacity = "1"; 
         details.style.visibility = "visible";
         details.style.position = "static"; // Reset to default position value
+
+        // Hide other logos
+        allLogos.forEach(logo => {
+            if (logo !== element) {
+                logo.style.display = 'none';
+            }
+        });
     } else {
         details.style.opacity = "0"; 
         details.style.visibility = "hidden";
         setTimeout(() => {
             details.style.position = "absolute"; // Delay the position change to after the fade out
         }, 500); // 500ms matches the transition duration
+
+        // Show all logos
+        allLogos.forEach(logo => {
+            logo.style.display = 'block';
+        });
     }
 }
 
